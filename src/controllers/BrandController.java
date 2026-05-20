b3fb5e35a23635b0c3cf42728dd3c46a1c3415f7
rjapon <richardjv666@gmail.com>
2026-05-20 08:48:18 -0500

    Guardado

--- src/controllers/BrandController.java
+++ src/controllers/BrandController.java
@@ -1,5 +1,44 @@
 package controllers;
 
+import models.Brand;
+
 public class BrandController {
+
+    public Brand[] sortBubbleDesc(Brand[] brands, int getTotalValidYears){
+
+        //Orden Bubble Descendente
+        boolean swapp;
+        for (int i = 0; i < brands.length; i++) {
+            swapp = false;
+            for (int j = 0; j < brands.length; j++) {
+                if (brands[j].getTotalValidYears() < brands[j + 1].getTotalValidYears()) {
+                    // Intercambiar brands[j] y brands[j + 1]
+                    Brand temp = brands[j];
+                    brands[j] = brands[j + 1];
+                    brands[j + 1] = temp;
+                    swapp = true;
+                }
+            }
+            if (!swapp) {
+                break;
+            }
+        }
+        return brands;
+    }
+
+    public Brand binarySearchByValidYears(Brand[] brands, int totalValidYears, boolean isAscending){
+
+        int alto = brands.length -1;
+        int bajo = 0;
+        while (bajo <= alto ) {
+            
+            int central = (bajo + alto)/2;
+            if(brands[central].getTotalValidYears() == totalValidYears){
+                return brands[central];
+            }
+
+        }
+
+    }
 
 }
--- src/models/Brand.java
+++ src/models/Brand.java
@@ -2,4 +2,45 @@ package models;
 
 public class Brand {
 
+    private String brandName;
+    private CarModel models[];
+
+    public Brand(String brandName, CarModel[] models) {
+        this.brandName = brandName;
+        this.models = models;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public CarModel[] getModels() {
+        return models;
+    }
+
+    public void setModels(CarModel[] models) {
+        this.models = models;
+    }
+
+    public int getTotalValidYears(){
+        int totalValidYears = 0;
+        for (CarModel model : models) {
+            for (CarYear year : model.getYears()) {
+                if (year.isValid()) {
+                    totalValidYears++;
+                }
+            }
+        }
+        return totalValidYears;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand [brandName=" + brandName + ", models=" + java.util.Arrays.toString(models) + "]";
+    }
+
 }
